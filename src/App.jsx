dc4951d781f5471ef535df7199a3f9ab57519711
sky-web-study-create <sky.webstudy@gmail.com>
2022-04-14 21:01:39 +0900

    Reactでビンゴシート作成（初回コミット）

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import "./App.css";
+//lodash(配列の縦横入替に使用)
+import lodash from "lodash";
+
+export const App = () => {
+  //BINGOのタイトル表示
+  const heads = ["B", "I", "N", "G", "O"];
+  const headsName = heads.map((head) => <th>{head}</th>);
+
+  const createColumn = (col) => {
+    const source = [];
+    //bingo各列に入りうる数字を配列に入れる
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
+
+    const column = [];
+
+    //source配列の数値をランダムに取り出して格納
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.random() * source.length, 1)[0];
+    }
+
+    return column;
+  };
+
+  const bingoDataCreate = () => {
+    const columns = [];
+
+    //bingoの数値を入れる
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+
+    //bingoの配列の真ん中は'FREE'にする
+    columns[2][2] = "FREE";
+
+    return columns;
+  };
+
+  const bingoColumns = bingoDataCreate();
+  //配列の縦横を入替
+  const bingoData = lodash.unzip(bingoColumns);
+  const bodyData = bingoData.map((row1, index) => (
+    <tr key={index}>
+      {row1.map((row2, index) => (
+        <td key={index}>{row2}</td>
+      ))}
+    </tr>
+  ));
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>{headsName}</tr>
+        </thead>
+        <tbody>
+          {bodyData}
+        </tbody>
+      </table>
+    </>
+  );
+};
